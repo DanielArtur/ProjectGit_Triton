d3320a9857b772a61d200d4345fe7a6cfd45019a
DanielArtur <pineco222@gmail.com>
2025-05-30 18:02:35 +0300

    AirMovement added and stuck in wall problem partially solved

--- Project_Triton/Assets/Scripts/PlayerControl/PlayerMovement.cs
+++ Project_Triton/Assets/Scripts/PlayerControl/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody rb;
     PlayerInput playerInput;
     InputAction moveAction;
+    PlayerStateChecker stateChecker;
+
 
     [Header("Refrences")]
     public Transform orientation;
@@ -17,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Player Atributes")]
     [SerializeField] float directionControl = 8;
+    [SerializeField] float inAirControl = 3;
     [SerializeField] float moveSpeed;
 
 
@@ -45,7 +48,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions["Move"];
-
+        stateChecker = GetComponent<PlayerStateChecker>();
     }
 
 
@@ -58,7 +61,17 @@ public class PlayerMovement : MonoBehaviour
         verticalInput = moveAction.ReadValue<Vector2>().y;
         horizontalInput = moveAction.ReadValue<Vector2>().x;
 
-        MovePlayer();
+        if (stateChecker.CurrentState == PlayerStateChecker.PlayerStates.OnGround)
+        {
+            MovePlayer();
+        }
+
+        else if (stateChecker.CurrentState == PlayerStateChecker.PlayerStates.InAir)
+        {
+            MovePlayerInAir();
+        }
+
+
     }
 
     ////////////////////////////////////////////////////////////////MovePlayer////////////////////////////////////////////////////////////////////
@@ -83,7 +96,25 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+    private void MovePlayerInAir()
+    {
 
+        yRot = new Vector3(orientation.forward.x, 0, orientation.forward.z);
+        yRot = yRot.normalized;
+
+        moveDirection = (yRot * verticalInput) + (orientation.right * horizontalInput);
+
+        moveDirection = moveDirection * moveSpeed;
+
+        moveDirection.y = rb.linearVelocity.y;
+
+
+        float inAirAcceleration = inAirControl * AdjustmentAmt;
+        lerpVelocityOfMovement = Vector3.Lerp(rb.linearVelocity, moveDirection, inAirAcceleration * Time.fixedDeltaTime);
+
+        rb.linearVelocity = lerpVelocityOfMovement;
+
+    }
     ////////////////////////////////////////////////////////////////Debug methods////////////////////////////////////////////////////////////////////
 
     private void OnDrawGizmos()
